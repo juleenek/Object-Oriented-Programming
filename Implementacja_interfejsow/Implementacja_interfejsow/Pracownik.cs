cca168cf6a91d4ae0e73970492e6f897a26ffc09
metaksa1 <julia.linek@microsoft.wsei.edu.pl>
2022-03-17 12:58:24 +0100

    Ad. 1-6

---
+++ Implementacja_interfejsow/Implementacja_interfejsow/Pracownik.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Implementacja_interfejsow
+{
+    class Pracownik
+    {
+        private string _nazwisko;
+        private DateTime _dataZatrudnienia;
+        private decimal _wynagrodzenie;
+
+        public string Nazwisko {
+            get => _nazwisko;  
+            set => _nazwisko = value.Trim(); 
+        }
+
+        public DateTime DataZatrudnienia {
+            get => _dataZatrudnienia;
+            set
+            {
+                DateTime thisDay = DateTime.Today;
+                if (value <= thisDay) _dataZatrudnienia = value; else throw new ArgumentException();
+            }
+        }
+
+        public decimal Wynagrodzenie {
+            get => _wynagrodzenie;
+            set => _wynagrodzenie = (value < 0) ? 0 : value;
+        }
+
+        public int CzasZatrudnienia => (DateTime.Now - DataZatrudnienia).Days / 30;
+
+        public Pracownik(string nazwisko, DateTime dataZatrudnienia, decimal wynagrodzenie)
+        {
+            Nazwisko = nazwisko;
+            DataZatrudnienia = dataZatrudnienia;
+            Wynagrodzenie = wynagrodzenie;
+        }
+
+        public Pracownik()
+        {
+            DateTime thisDay = DateTime.Today;
+
+            _nazwisko = "Anonim";
+            _dataZatrudnienia = thisDay;
+            _wynagrodzenie = 0;
+        }
+
+        public override string ToString() => $"({Nazwisko}, {DataZatrudnienia:d MMM yyyy}, ({CzasZatrudnienia}) {Wynagrodzenie} PLN)";
+    }
+}
---
+++ Implementacja_interfejsow/Implementacja_interfejsow/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Implementacja_interfejsow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var p1 = new Pracownik();
+            Console.WriteLine(p1);
+            p1.Wynagrodzenie = 100;
+            p1.Nazwisko = "   Molenda  ";
+            p1.DataZatrudnienia = new DateTime(2010, 10, 01);
+            Console.WriteLine(p1);
+
+            var p2 = new Pracownik("Abacki", new DateTime(2011, 8, 9), 200);
+            Console.WriteLine(p2);
+        }
+    }
+}
+
