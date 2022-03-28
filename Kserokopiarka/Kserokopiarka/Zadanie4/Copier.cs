fa22178107ba0ea08a6c680a62d254b5576cb564
Julia Linek <julia.linek@microsoft.wsei.edu.pl>
2022-03-28 23:18:19 +0200

    Start Zadanie4, fun with diagrams

---
+++ Kserokopiarka/Kserokopiarka/Zadanie4/Copier.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kserokopiarka.Zadanie4
+{
+    public class Copier : BaseDevice, IPrinter, IScanner{
+
+        public int PrintCounter { get; set; } 
+        public int ScanCounter { get; set; } 
+        public new int Counter { get; set; } 
+
+        public void Print(in IDocument document)
+        {
+            if (state == IDevice.State.off) 
+            {
+                Console.WriteLine("You need to turn on the device.");
+                return;
+            }
+                
+            Console.WriteLine($"{DateTime.Now} Print: {document.GetFileName()}");
+            PrintCounter++;      
+        }
+
+        public void Scan(out IDocument document, IDocument.FormatType formatType = IDocument.FormatType.JPG)
+        {
+            if (state == IDevice.State.off)
+            {
+                Console.WriteLine("You need to turn on the device.");
+                document = null;
+                return; 
+            }
+
+            string fileName;
+            ScanCounter++;
+
+            if (formatType == IDocument.FormatType.PDF)
+            {
+               fileName = $"PDFScan{Counter }.pdf";
+               document = new PDFDocument(fileName);
+               Console.WriteLine($"{DateTime.Now} Scan: {fileName}");
+                return;
+
+            } else if (formatType == IDocument.FormatType.JPG)
+            {
+                fileName = $"ImageScan{Counter}.jpg";
+                document = new ImageDocument(fileName);
+                Console.WriteLine($"{DateTime.Now} Scan: {fileName}");
+                return;
+
+            }else
+            {
+                fileName = $"TextScan{Counter}.txt";
+                document = new TextDocument(fileName);
+                Console.WriteLine($"{DateTime.Now} Scan: {fileName}");
+                return;
+
+            };
+        }
+
+        public new void PowerOn()
+        {
+            if (state == IDevice.State.on)
+            {
+                Console.WriteLine("Device is already on.");
+                return; 
+            }
+            Counter++;
+            state = IDevice.State.on;
+            Console.WriteLine("Device is on ...");
+        }
+        public new void PowerOff()
+        {
+            if (state == IDevice.State.off)
+            {
+                Console.WriteLine("Device is already off.");
+                return; 
+            }
+            state = IDevice.State.off;
+            Console.WriteLine("... Device is off!");
+        }
+
+        public void ScanAndPrint()
+        {
+            if (state == IDevice.State.off)
+            {
+                Console.WriteLine("You need to turn on the device.");
+                return; 
+            }
+
+            Scan(out IDocument document);
+            Print(in document);
+            
+        }
+    }
+}
---
+++ Kserokopiarka/Kserokopiarka/Zadanie4/Devices.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kserokopiarka.Zadanie4
+{
+    public interface IDevice
+    {
+        enum State { on, off, standby };
+
+        void PowerOn() // uruchamia urządzenie, zmienia stan na `on`
+        { 
+            SetState(State.on);
+        }
+        void PowerOff() // wyłącza urządzenie, zmienia stan na `off
+        {
+            SetState(State.off);
+        }
+        void StandbyOn() // uruchamia oszczędzanie energii gdy urządzenie jest włączone 
+        {
+            if (GetState() != State.on)
+            {
+                GetState();
+            }
+            else
+            {
+                SetState(State.standby);
+            }
+        }
+        void StandbyOff() // wyłącza oszczędzanie energii, ustawia State.on
+        {
+            if (GetState() != State.standby)
+            {
+                GetState();
+            }
+            else
+            {
+                SetState(State.on);
+            }
+        }
+
+        State GetState(); // zwraca aktualny stan urządzenia
+        abstract protected void SetState(State state); // ustawia stan oszczędzania energii
+
+        int Counter { get; }  // zwraca liczbę charakteryzującą eksploatację urządzenia,
+                              // np. liczbę uruchomień, liczbę wydrukow, liczbę skanów, ...
+    }
+
+    public abstract class BaseDevice : IDevice
+    {
+        protected IDevice.State state = IDevice.State.off;
+        public IDevice.State GetState() => state;
+
+        public void PowerOff()
+        {
+            state = IDevice.State.off;
+            Console.WriteLine("... Device is off !");
+        }
+
+        public void PowerOn()
+        {
+            state = IDevice.State.on;
+            Console.WriteLine("Device is on ...");
+        }
+
+        public int Counter { get; private set; } = 0;
+    }
+
+    public interface IPrinter : IDevice
+    {
+        /// <summary>
+        /// Dokument jest drukowany, jeśli urządzenie włączone. W przeciwnym przypadku nic się nie wykonuje
+        /// </summary>
+        /// <param name="document">obiekt typu IDocument, różny od `null`</param>
+        void Print(in IDocument document);
+    }
+
+    public interface IScanner : IDevice
+    {
+        // dokument jest skanowany, jeśli urządzenie włączone
+        // w przeciwnym przypadku nic się dzieje
+        void Scan(out IDocument document, IDocument.FormatType formatType);
+    }
+}
