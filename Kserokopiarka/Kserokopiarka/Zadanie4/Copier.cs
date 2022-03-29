2229e2546d05ac901cc0fdeaf40376451e019f2e
Julia Linek <julia.linek@microsoft.wsei.edu.pl>
2022-03-29 15:48:14 +0200

    NotImplemented Copier

--- Kserokopiarka/Kserokopiarka/Zadanie4/Copier.cs
+++ Kserokopiarka/Kserokopiarka/Zadanie4/Copier.cs
@@ -3,96 +3,47 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Threading;
 
 namespace Kserokopiarka.Zadanie4
 {
-    public class Copier : BaseDevice, IPrinter, IScanner{
+    public class Copier : IPrinter, IScanner
+    {
+        public int Counter => throw new NotImplementedException();
+        public Copier()
+        {
 
-        public int PrintCounter { get; set; } 
-        public int ScanCounter { get; set; } 
-        public new int Counter { get; set; } 
+        }
+
+        public IDevice.State GetState()
+        {
+            throw new NotImplementedException();
+        }
 
         public void Print(in IDocument document)
         {
-            if (state == IDevice.State.off) 
-            {
-                Console.WriteLine("You need to turn on the device.");
-                return;
-            }
-                
-            Console.WriteLine($"{DateTime.Now} Print: {document.GetFileName()}");
-            PrintCounter++;      
+            throw new NotImplementedException();
         }
 
-        public void Scan(out IDocument document, IDocument.FormatType formatType = IDocument.FormatType.JPG)
+        public void Scan(out IDocument document, IDocument.FormatType formatType)
         {
-            if (state == IDevice.State.off)
-            {
-                Console.WriteLine("You need to turn on the device.");
-                document = null;
-                return; 
-            }
-
-            string fileName;
-            ScanCounter++;
-
-            if (formatType == IDocument.FormatType.PDF)
-            {
-               fileName = $"PDFScan{Counter }.pdf";
-               document = new PDFDocument(fileName);
-               Console.WriteLine($"{DateTime.Now} Scan: {fileName}");
-                return;
-
-            } else if (formatType == IDocument.FormatType.JPG)
-            {
-                fileName = $"ImageScan{Counter}.jpg";
-                document = new ImageDocument(fileName);
-                Console.WriteLine($"{DateTime.Now} Scan: {fileName}");
-                return;
-
-            }else
-            {
-                fileName = $"TextScan{Counter}.txt";
-                document = new TextDocument(fileName);
-                Console.WriteLine($"{DateTime.Now} Scan: {fileName}");
-                return;
-
-            };
+            throw new NotImplementedException();
         }
-
-        public new void PowerOn()
+        void IDevice.SetState(IDevice.State state)
         {
-            if (state == IDevice.State.on)
-            {
-                Console.WriteLine("Device is already on.");
-                return; 
-            }
-            Counter++;
-            state = IDevice.State.on;
-            Console.WriteLine("Device is on ...");
-        }
-        public new void PowerOff()
-        {
-            if (state == IDevice.State.off)
-            {
-                Console.WriteLine("Device is already off.");
-                return; 
-            }
-            state = IDevice.State.off;
-            Console.WriteLine("... Device is off!");
+            throw new NotImplementedException();
         }
 
-        public void ScanAndPrint()
+        void IPrinter.SetState(IDevice.State state)
         {
-            if (state == IDevice.State.off)
-            {
-                Console.WriteLine("You need to turn on the device.");
-                return; 
-            }
-
-            Scan(out IDocument document);
-            Print(in document);
-            
+            throw new NotImplementedException();
         }
+
+        void IScanner.SetState(IDevice.State state)
+        {
+            throw new NotImplementedException();
+        }
+
     }
 }
--- Kserokopiarka/Kserokopiarka/Zadanie4/Devices.cs
+++ Kserokopiarka/Kserokopiarka/Zadanie4/Devices.cs
@@ -20,25 +20,11 @@ namespace Kserokopiarka.Zadanie4
         }
         void StandbyOn() // uruchamia oszczędzanie energii gdy urządzenie jest włączone 
         {
-            if (GetState() != State.on)
-            {
-                GetState();
-            }
-            else
-            {
-                SetState(State.standby);
-            }
+                SetState(State.standby);   
         }
         void StandbyOff() // wyłącza oszczędzanie energii, ustawia State.on
         {
-            if (GetState() != State.standby)
-            {
-                GetState();
-            }
-            else
-            {
                 SetState(State.on);
-            }
         }
 
         State GetState(); // zwraca aktualny stan urządzenia
@@ -48,26 +34,6 @@ namespace Kserokopiarka.Zadanie4
                               // np. liczbę uruchomień, liczbę wydrukow, liczbę skanów, ...
     }
 
-    public abstract class BaseDevice : IDevice
-    {
-        protected IDevice.State state = IDevice.State.off;
-        public IDevice.State GetState() => state;
-
-        public void PowerOff()
-        {
-            state = IDevice.State.off;
-            Console.WriteLine("... Device is off !");
-        }
-
-        public void PowerOn()
-        {
-            state = IDevice.State.on;
-            Console.WriteLine("Device is on ...");
-        }
-
-        public int Counter { get; private set; } = 0;
-    }
-
     public interface IPrinter : IDevice
     {
         /// <summary>
@@ -75,6 +41,7 @@ namespace Kserokopiarka.Zadanie4
         /// </summary>
         /// <param name="document">obiekt typu IDocument, różny od `null`</param>
         void Print(in IDocument document);
+        new void SetState(State state);
     }
 
     public interface IScanner : IDevice
@@ -82,5 +49,6 @@ namespace Kserokopiarka.Zadanie4
         // dokument jest skanowany, jeśli urządzenie włączone
         // w przeciwnym przypadku nic się dzieje
         void Scan(out IDocument document, IDocument.FormatType formatType);
+        new void SetState(State state);
     }
 }
