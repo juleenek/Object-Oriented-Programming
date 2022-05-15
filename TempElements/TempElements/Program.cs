01ec03642fd2bdd972706e93e7a6f28cb256ad85
Julia Linek <julia.linek@microsoft.wsei.edu.pl>
2022-05-15 17:52:53 +0200

    Zadanie 1

--- TempElements/TempElements/Program.cs
+++ TempElements/TempElements/Program.cs
@@ -1,12 +1,49 @@
 ﻿using System;
+using TempElementsLib.src;
 
 namespace TempElements
 {
     class Program
     {
+        static void Zadanie1()
+        {
+            using (var tmpFile = new TempFile("ccc.tmp"))
+            {
+                // Use temporary file stream ...
+                tmpFile.AddText("\\test");
+                tmpFile.fileStream.WriteByte(63);
+            }
+            // tmpFile is out of scope, resources disposed
+
+            // Correct, but not recommended
+            var tmpFile2 = new TempFile("ccc.tmp");
+            // Use temporary file stream ...
+            tmpFile2.Dispose();
+
+            //tmpFile2.AddText("\\test");
+            tmpFile2.fileStream.WriteByte(63);
+        }
+
+        static void Zadanie1_Try()
+        {
+            TempFile tmpFile = new TempFile("ccc.tmp");
+            try
+            {
+                tmpFile.AddText("\\test");
+                tmpFile.fileStream.WriteByte(63);
+                tmpFile.Dispose();
+                tmpFile.AddText("\\test"); // An exception will be thrown
+            }
+            catch (ObjectDisposedException exception)
+            {
+                Console.WriteLine($"{exception.Message} Zgłoszony wyjątek, nie można uzyskać dostępu do usuniętego pliku");
+            }
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Zadanie1();
+            Zadanie1_Try();
         }
     }
 }
+ 
---
+++ TempElements/TempElementsLib/src/TempFile.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TempElementsLib.src.Interfaces;
+
+namespace TempElementsLib.src
+{
+    public class TempFile : ITempFile
+    {
+        public readonly FileStream fileStream;
+        public readonly FileInfo fileInfo;
+        public bool IsDestroyed => !fileInfo.Exists;
+        public string FilePath => fileInfo.FullName;
+        public TempFile(string fileName)
+        {
+            fileInfo = new FileInfo(fileName);
+            fileStream = new FileStream(
+                FilePath, FileMode.OpenOrCreate,
+                FileAccess.ReadWrite);
+            Console.WriteLine($"File {FilePath} created");
+        }
+
+        public void AddText(string value)
+        {
+            byte[] info = new UTF8Encoding(true).GetBytes(value);
+            fileStream.Write(info, 0, info.Length);
+            fileStream.Flush();
+        }
+
+        public void Dispose()
+        {
+            if (!IsDestroyed) fileStream.Dispose();
+            if (!IsDestroyed) fileInfo.Delete();
+
+            Console.WriteLine($"File disposed");
+        }
+    }
+}
