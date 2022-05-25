f495523f079d5ad37c0145a7f626bd98177f5cb9
Julia Linek <julia.linek@microsoft.wsei.edu.pl>
2022-05-25 17:43:29 +0200

    Sorting

--- LittleExercises/Frekwencja_znakow/Frekwencja_znakow/Frekwencja_znakow/Program.cs
+++ LittleExercises/Frekwencja_znakow/Frekwencja_znakow/Frekwencja_znakow/Program.cs
@@ -7,21 +7,168 @@ namespace Frekwencja_znakow
 {
     class Program
     {
-        static Dictionary<char, int> SortByFreqency(string levels, Dictionary<char, int> inputDictionary)
+        static Dictionary<char, int> SortByFreqency(string levels, Dictionary<char, int> inputDictionary, string secondSort = "", string secondLevels = "")
         {
-            switch (levels)
+            if(secondSort == "byfreq" || secondSort == "byletter")
             {
-                case "asc":
-                    var result1 = from letters in inputDictionary orderby letters.Value ascending select letters;
-                    return result1.ToDictionary(pair => pair.Key, pair => pair.Value);
-                case "desc":
-                    var result2 = from letters in inputDictionary orderby letters.Value descending select letters;
-                    return result2.ToDictionary(pair => pair.Key, pair => pair.Value);
+                if(secondSort == "byfreq")
+                {
+                    switch (levels)
+                    {
+                        case "asc":
+                            if(secondLevels == "asc")
+                            {
+                                var result1 = from letters in inputDictionary orderby letters.Value ascending, letters.Value ascending select letters;
+                                return result1.ToDictionary(pair => pair.Key, pair => pair.Value);
+                            }
+                            if (secondLevels == "desc")
+                            {
+                                var result1 = from letters in inputDictionary orderby letters.Value ascending, letters.Value descending select letters;
+                                return result1.ToDictionary(pair => pair.Key, pair => pair.Value);
+                            }
+                            break;
+                        case "desc":
+                            if (secondLevels == "asc")
+                            {
+                                var result2 = from letters in inputDictionary orderby letters.Value descending, letters.Value ascending select letters;
+                                return result2.ToDictionary(pair => pair.Key, pair => pair.Value);
+                            }
+                            if (secondLevels == "desc")
+                            {
+                                var result2 = from letters in inputDictionary orderby letters.Value descending, letters.Value descending select letters;
+                                return result2.ToDictionary(pair => pair.Key, pair => pair.Value);
+                            }
+                            break;
+                    }
+                }
+                if (secondSort == "byletter")
+                {
+                    switch (levels)
+                    {
+                        case "asc":
+                            if (secondLevels == "asc")
+                            {
+                                var result1 = from letters in inputDictionary orderby letters.Value ascending, letters.Key ascending select letters;
+                                return result1.ToDictionary(pair => pair.Key, pair => pair.Value);
+                            }
+                            if (secondLevels == "desc")
+                            {
+                                var result1 = from letters in inputDictionary orderby letters.Value ascending, letters.Key descending select letters;
+                                return result1.ToDictionary(pair => pair.Key, pair => pair.Value);
+                            }
+                            break;
+                        case "desc":
+                            if (secondLevels == "asc")
+                            {
+                                var result2 = from letters in inputDictionary orderby letters.Value descending, letters.Key ascending select letters;
+                                return result2.ToDictionary(pair => pair.Key, pair => pair.Value);
+                            }
+                            if (secondLevels == "desc")
+                            {
+                                var result2 = from letters in inputDictionary orderby letters.Value descending, letters.Key descending select letters;
+                                return result2.ToDictionary(pair => pair.Key, pair => pair.Value);
+                            }
+                            break;
+                    }
+                }
+            }
+            if (secondSort == "" && secondSort == "")
+            {
+                switch (levels)
+                {
+                    case "asc":
+                        var result1 = from letters in inputDictionary orderby letters.Value ascending select letters;
+                        return result1.ToDictionary(pair => pair.Key, pair => pair.Value);
+                    case "desc":
+                        var result2 = from letters in inputDictionary orderby letters.Value descending select letters;
+                        return result2.ToDictionary(pair => pair.Key, pair => pair.Value);
+                }
+            }
+
+
+            return inputDictionary;
+        }
+        static Dictionary<char, int> SortByLetter(string levels, Dictionary<char, int> inputDictionary, string secondSort = "", string secondLevels = "")
+        {
+            if (secondSort == "byfreq" || secondSort == "byletter")
+            {
+                if (secondSort == "byfreq")
+                {
+                    switch (levels)
+                    {
+                        case "asc":
+                            if(secondLevels == "asc")
+                            {
+                                var result1 = from letters in inputDictionary orderby letters.Key ascending, letters.Value ascending select letters;
+                                return result1.ToDictionary(pair => pair.Key, pair => pair.Value);
+                            }
+                            if(secondLevels == "desc")
+                            {
+                                var result1 = from letters in inputDictionary orderby letters.Key ascending, letters.Value descending select letters;
+                                return result1.ToDictionary(pair => pair.Key, pair => pair.Value);
+                            }
+                            break;
+                        case "desc":
+                            if (secondLevels == "asc")
+                            {
+                                var result2 = from letters in inputDictionary orderby letters.Key descending, letters.Value ascending select letters;
+                                return result2.ToDictionary(pair => pair.Key, pair => pair.Value);
+                            }
+                            if (secondLevels == "desc")
+                            {
+                                var result2 = from letters in inputDictionary orderby letters.Key descending, letters.Value descending select letters;
+                                return result2.ToDictionary(pair => pair.Key, pair => pair.Value);
+                            }
+                            break;                         
+                    }
+                }
+                if(secondSort == "byletter")
+                {
+                    switch (levels)
+                    {
+                        case "asc":
+                            if (secondLevels == "asc")
+                            {
+                                var result1 = from letters in inputDictionary orderby letters.Key ascending, letters.Key ascending select letters;
+                                return result1.ToDictionary(pair => pair.Key, pair => pair.Value);
+                            }
+                            if (secondLevels == "desc")
+                            {
+                                var result1 = from letters in inputDictionary orderby letters.Key ascending, letters.Key descending select letters;
+                                return result1.ToDictionary(pair => pair.Key, pair => pair.Value);
+                            }
+                            break;
+                        case "desc":
+                            if (secondLevels == "asc")
+                            {
+                                var result2 = from letters in inputDictionary orderby letters.Key descending, letters.Key ascending select letters;
+                                return result2.ToDictionary(pair => pair.Key, pair => pair.Value);
+                            }
+                            if (secondLevels == "desc")
+                            {
+                                var result2 = from letters in inputDictionary orderby letters.Key descending, letters.Key descending select letters;
+                                return result2.ToDictionary(pair => pair.Key, pair => pair.Value);
+                            }
+                            break;
+                    }
+                }
+            }
+
+            if (secondSort == "" && secondSort == "")
+            {
+                switch (levels)
+                {
+                    case "asc":
+                        var result1 = from letters in inputDictionary orderby letters.Key ascending select letters;
+                        return result1.ToDictionary(pair => pair.Key, pair => pair.Value);
+                    case "desc":
+                        var result2 = from letters in inputDictionary orderby letters.Key descending select letters;
+                        return result2.ToDictionary(pair => pair.Key, pair => pair.Value);
+                }
             }
 
             return inputDictionary;
         }
-
         static void Main(string[] args)
         {
             int testCases = int.Parse(Console.ReadLine());
@@ -32,7 +179,6 @@ namespace Frekwencja_znakow
                 string queryInput = Console.ReadLine();
 
                 List<char> letters = new List<char>();
-                //List<string> query = new List<string>();
                 var query = queryInput.Split(' ');
 
                 for (int j = 0; j < text.Length; j++)
@@ -48,8 +194,25 @@ namespace Frekwencja_znakow
                 switch (query[2])
                 {
                     case "byfreq":
+                        if (4 < query.Length && 5 < query.Length)
+                        {
+                            if (query[3] == "asc") firstSort = SortByFreqency("asc", results, query[4], query[5]);
+                            if (query[3] == "desc") firstSort = SortByFreqency("desc", results, query[4], query[5]);
+                            break;
+                        }
                         if (query[3] == "asc") firstSort = SortByFreqency("asc", results);
                         if (query[3] == "desc") firstSort = SortByFreqency("desc", results);
+                        break;
+
+                    case "byletter":
+                        if (4 < query.Length && 5 < query.Length)
+                        {
+                            if (query[3] == "asc") firstSort = SortByLetter("asc", results, query[4], query[5]);
+                            if (query[3] == "desc") firstSort = SortByLetter("desc", results, query[4], query[5]);
+                            break;
+                        }
+                        if (query[3] == "asc") firstSort = SortByLetter("asc", results);
+                        if (query[3] == "desc") firstSort = SortByLetter("desc", results);
                         break;
                 }
 
